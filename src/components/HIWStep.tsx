2d81f438625abb8d3a9a99848b202f8aa5a722de
Jackson Kalmbach <jacksonrkalmbach@gmail.com>
2024-01-24 16:57:32 -0600

    responsive styling for how it works section

--- src/components/HIWStep.tsx
+++ src/components/HIWStep.tsx
@@ -22,27 +22,31 @@ const HIWStep = ({ step, currentStep, handleStep }: Props) => {
       key={stepNumber}
       onClick={() => handleStep(stepNumber)}
       className={`${
-        stepNumber === currentStep ? "bg-bg-light" : "opacity-40"
-      } p-4 rounded-xl cursor-pointer`}
+        stepNumber === currentStep
+          ? "bg-bg-light rounded-xl p-4"
+          : "opacity-40 flex items-center w-full px-4"
+      }  cursor-pointer flex flex-col`}
     >
-      <div className="flex w-full items-center justify-between">
-        <div className="flex items-center gap-2 md:gap-4 ">
+      <div className="flex w-full items-center justify-start">
+        <div className="flex w-full items-center gap-2 mb-2">
           <div className="h-8 w-8 md:h-10 md:w-10">{icon}</div>
-          <h2 className="text-xl md:text-2xl text-center">{title}</h2>
+          <h2 className="text-xl md:text-2xl text-center text-accent-blue font-medium">
+            {title}
+          </h2>
         </div>
       </div>
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-2 items-start">
         {description.map((desc: any, index: number) => {
           return (
             <div
               key={index}
               className={`${
                 currentStep === step.stepNumber
-                  ? "flex flex-col gap-1 mt-2 h-[80px] opacity-100 visible"
+                  ? "flex flex-col justify-center items-start h-[150px] md:h-[100px] opacity-100 visible"
                   : "opacity-0 invisible h-0"
               } transition-all duration-[250ms] ease-in`}
             >
-              <p className="font-medium">{desc.subtitle}</p>
+              <p className="font-medium text-lg">{desc.subtitle}</p>
               <p>{desc.note}</p>
             </div>
           );
--- src/components/HowItWorks.tsx
+++ src/components/HowItWorks.tsx
@@ -4,7 +4,10 @@ import {
   ShieldCheckIcon,
 } from "@heroicons/react/24/solid";
 import React, { useState } from "react";
+import wfhImg from "../assets/working-from-home.png";
 import HIWStep from "./HIWStep";
+import BlogIllustration1 from "./illustrations/BlogIllustration1";
+import Button from "./ui/Button";
 
 const steps = [
   {
@@ -58,24 +61,45 @@ const HowItWorks = () => {
   const [currentStep, setCurrentStep] = useState<number>(1);
 
   return (
-    <div className="w-screen h-3/4 flex px-6 py-8 items-center justify-around">
-      <div className="hidden lg:block w-[600px] bg-bg-light rounded-xl h-[500px]"></div>
-      <div className="flex flex-col lg:p-6 w-full lg:w-1/2">
-        <h1 className="text-3xl font-medium">How It Works</h1>
-        <p className="">
-          Discover Your Dream Home in Three Simple Steps with NestFinder
+    <div className="w-screen p-8">
+      <div className="relative w-full flex items-center justify-between">
+        <div className="hidden lg:block lg:w-[500px] xl:w-[600px] rounded-xl xl:h-[500px]">
+          <img src={wfhImg} alt="illustration" />
+        </div>
+        <div className="hidden lg:block rotate-90 absolute left-0 lg:h-[600px] xl:h-[800px] -z-10">
+          <BlogIllustration1 />
+        </div>
+        <div className="flex flex-col lg:p-6 w-full lg:w-1/2">
+          <h1 className="text-3xl lg:text-4xl font-medium text-center md:text-left">
+            How It Works
+          </h1>
+          <p className="lg:text-xl text-center md:text-left">
+            Discover Your Dream Home in Three Simple Steps with NestFinder
+          </p>
+          <div className="flex flex-col mt-6 gap-4">
+            {steps.map((step: any) => {
+              return (
+                <HIWStep
+                  step={step}
+                  handleStep={setCurrentStep}
+                  currentStep={currentStep}
+                  key={step.stepNumber}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </div>
+      <div className="w-full flex flex-col justify-center items-center gap-4 mt-12">
+        <p className="text-xl md:text-2xl text-center">
+          Ready to find your next apartment?
         </p>
-        <div className="flex flex-col mt-6 gap-4">
-          {steps.map((step: any) => {
-            return (
-              <HIWStep
-                step={step}
-                handleStep={setCurrentStep}
-                currentStep={currentStep}
-                key={step.stepNumber}
-              />
-            );
-          })}
+        <div className="w-full md:w-[200px]">
+          <Button
+            text="Join Now"
+            variant="primary"
+            onClick={() => console.log("get started")}
+          />
         </div>
       </div>
     </div>
